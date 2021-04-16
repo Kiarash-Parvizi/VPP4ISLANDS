e2ee2a749b6500846afc2fbd9c77de3f416b58a8
plutocholia <kiarash.nrz77@gmail.com>
2021-04-16 13:19:21 +0430

    UncertaintyParams class is added

---
+++ src/vppNode/UncertaintyParams.py
@@ -0,0 +1,38 @@
+class UncertaintyParams:
+    """ UncertaintyParams is a class which represents uncertainty parameters given by VPPBox
+    Attributes:
+    wf_pu       (float): P.U of WF
+    pv_pu       (float): P.U of PV
+    dg_pu       (float): P.U of Distributed Generators
+    da_price    (float): DA Price
+    rt_price    (float): RT Price
+    time        (float)
+    """
+
+    def __init__(self, wf_pu: float, pv_pu: float, dg_pu: float, da_price: float, rt_price: float, time: float):
+        self.wf_pu = wf_pu
+        self.pv_pu = pv_pu
+        self.dg_pu = dg_pu
+        self.da_price = da_price
+        self.rt_price = rt_price
+        self.time = time
+
+    def __str__(self):
+        return self.__class__.__name__ + ": { " + \
+               "\n\tP.U of WF: " + str(self.wf_pu) + \
+               "\n\tP.U of PV: " + str(self.pv_pu) + \
+               "\n\tP.U of DG: " + str(self.dg_pu) + \
+               "\n\tDA Price: " + str(self.da_price) + \
+               "\n\tRT Price: " + str(self.rt_price) + \
+               "\n\ttime    : " + str(self.time) + \
+               "\n}"
+
+    @staticmethod
+    def get_instance_by_json(item: dict):
+        wf_pu = item['WF']
+        pv_pu = item['PV']
+        dg_pu = item['DG']
+        da_price = item['DA_PRICE']
+        rt_price = item['RT_PRICE']
+        time = item['TIME']
+        return UncertaintyParams(wf_pu=wf_pu, pv_pu=pv_pu, dg_pu=dg_pu, da_price=da_price, rt_price=rt_price, time=time)
