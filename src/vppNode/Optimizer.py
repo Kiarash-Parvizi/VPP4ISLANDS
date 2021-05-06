ef432a67ca1da04ac8dfb35ed1f853c860d36c72
Kiarash-Parvizi <cdev.developer@gmail.com>
2021-05-06 16:48:52 +0430

    Optimizer::create_variables 1.1

--- src/vppNode/Optimizer.py
+++ src/vppNode/Optimizer.py
@@ -10,123 +10,52 @@ class Optimizer:
         # create gurobi model
         self.model = gp.Model("model1")
         # add model variable
-        self.__create_variables(NSb=1, NW=1, NT=24, NDG=10)
+        self.__create_variables(NW=1, NT=24)
         # set objective
         self.model.setObjective(GRB.MINIMIZE)
         # set constraints
         self.set_constraints()
 
-    def __create_variables(self, NW, NT, NSb, NDG):
+    def __create_variables(self, NW, NT):
         # var map
         self.var = {
-            'P': {
-                'DaBuy': {
-                    # ['P']['DaBuy'][1][1][24]
-                    sb: {
-                        w: {
-                            t: self.model.addVar(
-                                vtype=GRB.REAL,
-                                name='P_DaBuy_'+str(sb)+'_'+str(w)+'_'+str(t)
-                               )
-                            for t in range(1, NT+1)
-                        }
-                        for w in range(1, NW+1)
-                    }
-                    for sb in range(1, NSb+1)
-                },
-                'DaSell': {
-                    # ['P']['DaSell'][1][1][24]
-                    sb: {
-                        w: {
-                            t: self.model.addVar(
-                                vtype=GRB.REAL,
-                                name='P_DaSell_'+str(sb)+'_'+str(w)+'_'+str(t)
-                               )
-                            for t in range(1, NT+1)
-                        }
-                        for w in range(1, NW+1)
-                    }
-                    for sb in range(1, NSb+1)
-                },
-                'DG': {
-                    # ['P']['DG'][5][1][24]
-                    i: {
-                        w: {
-                            t: self.model.addVar(
-                                vtype=GRB.REAL,
-                                name='P_DG_'+str(i)+'_'+str(w)+'_'+str(t)
-                               )
-                            for t in range(1, NT+1)
-                        }
-                        for w in range(1, NW+1)
-                    }
-                    for i in range(1, NDG+1)
-                },
-            },
-            'Q': {
-                'DG': {
-                    # ['Q']['DG'][5][1][24]
-                    i: {
-                        w: {
-                            t: self.model.addVar(
-                                vtype=GRB.REAL,
-                                name='Q_DG_'+str(i)+'_'+str(w)+'_'+str(t)
-                               )
-                            for t in range(1, NT+1)
-                        }
-                        for w in range(1, NW+1)
-                    }
-                    for i in range(1, NDG+1)
-                },
-            },
-            'v': {
-                'DG': {
-                    'SU': {
-                        # ['v']['DG']['SU'][5][1][24]
-                        i: {
-                            w: {
-                                t: self.model.addVar(
-                                    vtype=GRB.REAL,
-                                    name='v_DG_SU_'+str(i)+'_'+str(w)+'_'+str(t)
-                                   )
-                                for t in range(1, NT+1)
-                            }
-                            for w in range(1, NW+1)
-                        }
-                        for i in range(1, NDG+1)
+            # format: w,t,['type'..],i
+            w: {
+                t: {
+                    'P': {
+                        'DA': {
+                            'buy': {},
+                            'sell': {},
+                        },
+                        'DG': {},
+                        'ChES' : {}, 
+                        'DchES': {},
+                        'flex': {},
+                        '+': {},
+                        '-': {},
                     },
-                    'SD': {
-                        # ['v']['DG']['SD'][5][1][24]
-                        i: {
-                            w: {
-                                t: self.model.addVar(
-                                    vtype=GRB.REAL,
-                                    name='v_DG_SD_'+str(i)+'_'+str(w)+'_'+str(t)
-                                   )
-                                for t in range(1, NT+1)
-                            }
-                            for w in range(1, NW+1)
-                        }
-                        for i in range(1, NDG+1)
+                    'Q': {
+                        'DG': {},
+                        'flex': {},
+                        '+': {},
+                        '-': {},
                     },
-                },
-            },
-            'U': {
-                'DG': {
-                    # ['U']['DG'][5][1][24]
-                    i: {
-                        w: {
-                            t: self.model.addVar(
-                                vtype=GRB.REAL,
-                                name='U_DG_'+str(i)+'_'+str(w)+'_'+str(t)
-                               )
-                            for t in range(1, NT+1)
-                        }
-                        for w in range(1, NW+1)
+                    'v': {
+                        'DG': {
+                            'SU': {},
+                            'SD': {},
+                        },
+                    },
+                    'U': {
+                        'DG': {},
+                    },
+                    'SOE': {
+                        'ES': {}
                     }
-                    for i in range(1, NDG+1)
-                },
+                }
+                for t in range(1, NT+1)
             }
+            for w in range(1, NW+1)
         }
 
     # uses the VppInterface to retrieve input values
