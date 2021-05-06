37b07fcba7e31df18b72caa109909a08522ce114
Kiarash-Parvizi <cdev.developer@gmail.com>
2021-05-06 15:36:23 +0430

    Optimizer::create_variables 0.1

--- src/vppNode/Optimizer.py
+++ src/vppNode/Optimizer.py
@@ -10,15 +10,83 @@ class Optimizer:
         # create gurobi model
         self.model = gp.Model("model1")
         # add model variable
-        # var['str'][i]
-        self.model.addVar(vtype= GRB.INTEGER, name="x")
+        self.__create_variables(NSb=1, NW=1, NT=24)
         # set objective
-        #self.model.setObjective(, GRB.MINIMIZE)
+        self.model.setObjective(GRB.MINIMIZE)
+        # set constraints
         self.set_constraints()
+
+    def __create_variables(self, NW, NT, NSb, NDG):
+        # var map
+        self.var = {
+            'P': {
+                'DaBuy': {
+                    # ['P']['DaBuy'][1][1][24]
+                    sb: {
+                        w: {
+                            t: self.model.addVar(
+                                vtype=GRB.REAL,
+                                name='P_DaBuy_'+str(sb)+'_'+str(w)+'_'+str(t)
+                               )
+                            for t in range(1, NT+1)
+                        }
+                        for w in range(1, NW+1)
+                    }
+                    for sb in range(1, NSb+1)
+                },
+                'DaSell': {
+                    # ['P']['DaSell'][1][1][24]
+                    sb: {
+                        w: {
+                            t: self.model.addVar(
+                                vtype=GRB.REAL,
+                                name='P_DaSell_'+str(sb)+'_'+str(w)+'_'+str(t)
+                               )
+                            for t in range(1, NT+1)
+                        }
+                        for w in range(1, NW+1)
+                    }
+                    for sb in range(1, NSb+1)
+                },
+                'DG': {
+                    # ['P']['DG'][5][1][24]
+                    i: {
+                        w: {
+                            t: self.model.addVar(
+                                vtype=GRB.REAL,
+                                name='P_DG_'+str(i)+'_'+str(w)+'_'+str(t)
+                               )
+                            for t in range(1, NT+1)
+                        }
+                        for w in range(1, NW+1)
+                    }
+                    for i in range(1, NDG+1)
+                },
+            },
+            'Q': {
+                'DG': {
+                    # ['Q']['DG'][5][1][24]
+                    i: {
+                        w: {
+                            t: self.model.addVar(
+                                vtype=GRB.REAL,
+                                name='Q_DG_'+str(i)+'_'+str(w)+'_'+str(t)
+                               )
+                            for t in range(1, NT+1)
+                        }
+                        for w in range(1, NW+1)
+                    }
+                    for i in range(1, NDG+1)
+                },
+            }
+        }
 
     # uses the VppInterface to retrieve input values
     def __fetch_data(self):
         self.dat = self.vppInterface.get_optimizer_input_data()
+
+    def set_objective():
+        pass
 
     def set_constraints(self):
         # remove old constraints
