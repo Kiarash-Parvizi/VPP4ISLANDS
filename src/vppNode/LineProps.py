abbb1ebb19aaf207fc70b28b84ff719c5b111809
plutocholia <kiarash.nrz77@gmail.com>
2021-05-18 04:09:57 +0430

    I max p.u is added

--- src/vppNode/LineProps.py
+++ src/vppNode/LineProps.py
@@ -2,18 +2,20 @@
 class LineProps:
     # instance variables
     # length: float
-    # resistance: float
-    # capacitance: float
-    # inductance: float
+    # resistance: float -> R (Ohm)
+    # capacitance: float -> X (Farad)
+    # inductance: float -> i max (A)
+    # i_max_pu: float -> i max (p.u)
     ###
 
-    def __init__(self, length, resistance, capacitance, inductance) -> None:
+    def __init__(self, length, resistance, capacitance, inductance, i_max_pu) -> None:
         self.length = length
         self.resistance  = resistance
         self.capacitance = capacitance
         self.inductance  = inductance
+        self.i_max_pu = i_max_pu
 
-    def calc_powerLoss() -> float:
+    def calc_powerLoss(self) -> float:
         ### calc power loss here
         return 1.2
         ###
