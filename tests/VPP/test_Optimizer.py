4b9cdad322a397947f06f6586c1faa8d1ba4bf13
Kiarash-Parvizi <cdev.developer@gmail.com>
2021-05-25 13:50:18 +0430

    VPP.test_Optimizer modified

--- tests/VPP/test_Optimizer.py
+++ tests/VPP/test_Optimizer.py
@@ -1,5 +1,7 @@
 from src.vppNode.Optimizer import Optimizer
 from .test_VppInterface import *
+import pprint
+import pyperclip
 
 # init
 optimizer = Optimizer(interf)
@@ -8,4 +10,8 @@ optimizer = Optimizer(interf)
 if __name__ == '__main__':
     optimizer.optimize()
     optimizer.distribute_results()
+    data = interf.get_graph_asJson()
+    if input('print data here: ').lower()[0] == 'y':
+        print(data)
+    pyperclip.copy(data)
     pass
