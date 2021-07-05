548104b9ea61acede3826f67e1274d226c3534b0
plutocholia <kiarash.nrz77@gmail.com>
2021-07-05 15:04:46 +0430

    old version of VppNodeModel is added with timestamp

--- src/Models/Models.py
+++ src/Models/Models.py
@@ -1,11 +1,13 @@
 from src.Models.Database import DBClient
 from src.vppNode.VppBoxNode import VppBoxNode
+from src.vppNode.VppNode import VppNode
 from src.PowerResources.DG import DG
 from src.PowerResources.PV import PV
 from src.PowerResources.ES import ES
 from src.PowerResources.FL import FL
 from src.PowerResources.WF import WF
-
+import json
+from datetime import datetime
 
 class BoxNodesModel:
     
@@ -17,8 +19,6 @@ class BoxNodesModel:
             database_name=self.database_name, collection_name=self.collection_name)
 
 
-    def get_boxnode_by_id(self, id: int) -> VppBoxNode:
-        pass
 
     def insert_boxnode(self, box_node: VppBoxNode) -> bool:
         _query = {"_id" : box_node.node_id}
@@ -27,7 +27,9 @@ class BoxNodesModel:
         if found_obj:
             print(f"node {box_node.node_id} already exists")
         else:
-            res = self.col.insert_one(box_node.to_dict())
+            json_data = json.dumps(box_node.to_dict())
+            _dict = json.loads(json_data)
+            res = self.col.insert_one(_dict)
         return True
     
     def update_boxnode(self, box_node: VppBoxNode) -> bool:
@@ -48,4 +50,17 @@ class BoxNodesModel:
         _query = {"_id" : _id}
         res = self.col.update_one(_query, {"$push", {'dgs' : dg.to_dict()}}, upsert=True)
         print(res)
-        
+
+class VppNodeModel:
+    def __init__(self) -> None:
+        self.client = DBClient()
+        self.database_name = "VPPNode"
+        self.collection_name = "VppNodes"
+        self.col = self.client.get_collection(
+            database_name=self.database_name, collection_name=self.collection_name)
+    
+    def insert_vppnode(self, vpp_node: VppNode) -> bool:
+        _dict = json.loads(json.dumps(vpp_node.to_dict()))
+        _dict['time'] = datetime.now()
+        res = self.col.insert_one(_dict)
+        return True
