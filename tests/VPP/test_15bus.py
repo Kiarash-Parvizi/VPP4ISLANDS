14d4ab24f4c6651d62b996e8dee024215317232f
plutocholia <kiarash.nrz77@gmail.com>
2021-08-19 15:43:47 +0430

    changing hard coded line info to file format

--- tests/VPP/test_15bus.py
+++ tests/VPP/test_15bus.py
@@ -4,6 +4,7 @@ from src.vppNode.Junction import Junction
 from typing import Dict
 from src.vppNode.LineProps import LineProps
 
+from src.FakeAPI.utils import read_nodes_structure
 
 NNodes = 15
 
@@ -36,79 +37,86 @@ for _, node in nodes.items():
 
 j_ids = {_id: vppNode.add_junction_by_id(_id, item) for _id, item in nodes.items()}
 
-connections = [
-    {
-        'nodes': (1, 2),
-        'line': LineProps(1.35309, 1.32349, 241.444, 2)
-    },
+# connections = [
+#     {   # l1
+#         'nodes': (1, 2),
+#         'line': LineProps(1.35309, 1.32349, 241.444, 2)
+#     },
 
-    {
-        'nodes': (2, 3),
-        'line': LineProps(1.17024, 1.14464, 228.16502204493, 1.89)
-    },
+#     {   # l2
+#         'nodes': (2, 3),
+#         'line': LineProps(1.17024, 1.14464, 228.16502204493, 1.89)
+#     },
 
-    {
-        'nodes': (2, 6),
-        'line': LineProps(2.55727, 1.7249, 241.444467772412, 2)
-    },
+#     {   # l7
+#         'nodes': (2, 6),
+#         'line': LineProps(2.55727, 1.7249, 241.444467772412, 2)
+#     },
 
-    {
-        'nodes': (2, 9),
-        'line': LineProps(2.01317, 1.3579, 152.834348099937, 1.266)
-    },
+#     {   # l12
+#         'nodes': (2, 9),
+#         'line': LineProps(2.01317, 1.3579, 152.834348099937, 1.266)
+#     },
 
-    {
-        'nodes': (3, 4),
-        'line': LineProps(0.84111, 0.82271, 228.16502204493, 1.89)
-    },
+#     {   # l3
+#         'nodes': (3, 4),
+#         'line': LineProps(0.84111, 0.82271, 228.16502204493, 1.89)
+#     },
 
-    {
-        'nodes': (3, 11),
-        'line': LineProps(1.79553, 1.2111, 152.834348099937, 1.266)
-    },
+#     {   # l10
+#         'nodes': (3, 11),
+#         'line': LineProps(1.79553, 1.2111, 152.834348099937, 1.266)
+#     },
 
-    {
-        'nodes': (4, 5),
-        'line': LineProps(1.52348, 1.0276, 152.834348099937, 1.266)
-    },
+#     {   # l4
+#         'nodes': (4, 5),
+#         'line': LineProps(1.52348, 1.0276, 152.834348099937, 1.266)
+#     },
 
-    {
-        'nodes': (4, 14),
-        'line': LineProps(2.23081, 1.5047, 152.834348099937, 1.266)
-    },
+#     {   # l13
+#         'nodes': (4, 14),
+#         'line': LineProps(2.23081, 1.5047, 152.834348099937, 1.266)
+#     },
 
-    {
-        'nodes': (4, 15),
-        'line': LineProps(1.19702, 0.8074, 152.834348099937, 1.266)
-    },
+#     {   # l14
+#         'nodes': (4, 15),
+#         'line': LineProps(1.19702, 0.8074, 152.834348099937, 1.266)
+#     },
 
-    {
-        'nodes': (6, 7),
-        'line': LineProps(1.0882, 0.734, 241.444467772412, 2)
-    },
+#     {   # l8
+#         'nodes': (6, 7),
+#         'line': LineProps(1.0882, 0.734, 241.444467772412, 2)
+#     },
 
-    {
-        'nodes': (6, 8),
-        'line': LineProps(1.25143, 0.8441, 241.444467772412, 2)
-    },
+#     {   # l9
+#         'nodes': (6, 8),
+#         'line': LineProps(1.25143, 0.8441, 241.444467772412, 2)
+#     },
 
-    {
-        'nodes': (9, 10),
-        'line': LineProps(1.68671, 1.1377, 152.834348099937, 1.266)
-    },
+#     {   # l6
+#         'nodes': (9, 10),
+#         'line': LineProps(1.68671, 1.1377, 152.834348099937, 1.266)
+#     },
 
-    {
-        'nodes': (11, 12),
-        'line': LineProps(2.44845, 1.6515, 152.834348099937, 1.266)
-    },
+#     {   # l11
+#         'nodes': (11, 12),
+#         'line': LineProps(2.44845, 1.6515, 152.834348099937, 1.266)
+#     },
 
-    {
-        'nodes': (12, 13),
-        'line': LineProps(2.01317, 1.3579, 152.834348099937, 1.266)
-    },
-]
+#     {   # l5
+#         'nodes': (12, 13),
+#         'line': LineProps(2.01317, 1.3579, 152.834348099937, 1.266)
+#     },
+# ]
+
+# connections_history = {}
+# for connection in connections:
+#     _id = vppNode.add_edge(connection['nodes'], connection['line'])
+#     connections_history[_id] = connection
+
+connections = read_nodes_structure()['data']
 
 connections_history = {}
 for connection in connections:
-    _id = vppNode.add_edge(connection['nodes'], connection['line'])
+    _id = vppNode.add_edge(connection['nodes'], LineProps(**connection['line']))
     connections_history[_id] = connection
